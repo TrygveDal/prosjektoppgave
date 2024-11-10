f6c86b3ac7d02140640ad731b86776d2e4cc6656
Trygve <148715358+TrygveDal@users.noreply.github.com>
2024-11-10 19:07:57 +0100

    Update db_filldata.sql

--- db_filldata.sql
+++ db_filldata.sql
@@ -4,8 +4,8 @@ INSERT INTO `Articles`(`views`) VALUES (0);
 INSERT INTO `Articles`(`views`) VALUES (0);
 INSERT INTO `Articles`(`views`) VALUES (0);
 INSERT INTO `Articles`(`views`) VALUES (0);
-INSERT INTO `Versions` (`author`,`content`,`edit_time`,`latest`,`pageId`,`title`,`type`,`versionnr`) VALUES ("Svein Rør","Denne artikkelen inneholder informasjon om temaet til artikkel 1",1,1,1,"Artikkel 1","init",1);
-INSERT INTO `Versions` (`author`,`content`,`edit_time`,`latest`,`pageId`,`title`,`type`,`versionnr`) VALUES ("Anne Katt","Denne artikkelen inneholder informasjon om temaet til artikkel 2",1,1,2,"Artikkel 2","init",1);
-INSERT INTO `Versions` (`author`,`content`,`edit_time`,`latest`,`pageId`,`title`,`type`,`versionnr`) VALUES ("Kjell Loke","Denne artikkelen inneholder informasjon om temaet til artikkel 3",1,1,3,"Artikkel 3","init",1);
-INSERT INTO `Versions` (`author`,`content`,`edit_time`,`latest`,`pageId`,`title`,`type`,`versionnr`) VALUES ("Ruben","Denne artikkelen inneholder informasjon om temaet til artikkel 4",1,1,4,"Artikkel 4","init",1);
-INSERT INTO `Versions` (`author`,`content`,`edit_time`,`latest`,`pageId`,`title`,`type`,`versionnr`) VALUES ("Kari","Denne artikkelen inneholder informasjon om temaet til artikkel 5",1,1,5,"Artikkel 5","init",1);
+INSERT INTO `Versions`(`article_id`, `title`, `content`, `author`, `version_type`, `edit_time`, `is_newest_version`) VALUES (1, "Artikkel 1", "Denne artikkelen inneholder informasjon om temaet til artikkel 1", "Svein Rør", "created", 1, 1)
+INSERT INTO `Versions`(`article_id`, `title`, `content`, `author`, `version_type`, `edit_time`, `is_newest_version`) VALUES (2, "Artikkel 2", "Denne artikkelen inneholder informasjon om temaet til artikkel 2", "Anne Katt", "created", 1, 1);
+INSERT INTO `Versions`(`article_id`, `title`, `content`, `author`, `version_type`, `edit_time`, `is_newest_version`) VALUES (3, "Artikkel 3", "Denne artikkelen inneholder informasjon om temaet til artikkel 3", "Kjell Loke", "created", 1, 1);
+INSERT INTO `Versions`(`article_id`, `title`, `content`, `author`, `version_type`, `edit_time`, `is_newest_version`) VALUES (4, "Artikkel 4", "Denne artikkelen inneholder informasjon om temaet til artikkel 4", "Ruben", "created", 1, 1);
+INSERT INTO `Versions`(`article_id`, `title`, `content`, `author`, `version_type`, `edit_time`, `is_newest_version`) VALUES (5, "Artikkel 5", "Denne artikkelen inneholder informasjon om temaet til artikkel 5", "Kari", "created", 1, 1);
